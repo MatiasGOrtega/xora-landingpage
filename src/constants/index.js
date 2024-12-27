0bbc966de6170e3de6c8e1ea378273a385f51937
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-12-27 20:58:14 -0300

    feat: restructure App component and add Features section with constants

---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+export const features = [
+  {
+    id: '0',
+    icon: '/images/feature-1.png',
+    caption: 'FÁCIL INTEGRACIÓN',
+    title: 'Trabaje de manera más inteligente, no más duro',
+    text: "Con Xora, las tareas tediosas son cosa del pasado. La automatización y los procesos inteligentes elevan tu productividad a nuevas cotas. Es como tomar una taza de café extra, pero sin nerviosismo.",
+    button: {
+      icon: '/images/magictouch.svg',
+      title: 'VER DEMO',
+    },
+  },
+  {
+    id: '1',
+    icon: '/images/feature-2.png',
+    caption: 'SEGURO Y CONFIABLE',
+    title: 'Duerme tranquilo, nosotros te respaldamos',
+    text: 'La seguridad de sus datos es nuestra prioridad. Con cifrado de última generación y controles de privacidad sólidos, Xora ayuda a mantener su información segura y protegida con más seguridad que Fort Knox.',
+    button: {
+      icon: '/images/docs.svg',
+      title: 'LEER DOCUMENTOS',
+    },
+  },
+]
+
+export const details = [
+  {
+    id: "0",
+    icon: "/images/detail-1.png",
+    title: "EDICIÓN DE VÍDEO AUTOMATIZADA CON IA",
+  },
+  {
+    id: "1",
+    icon: "/images/detail-2.png",
+    title: "COLABORA CON TU EQUIPO",
+  },
+  {
+    id: "2",
+    icon: "/images/detail-3.png",
+    title: "ULTRA RÁPIDO CON MOTOR EN LA NUBE",
+  },
+  {
+    id: "3",
+    icon: "/images/detail-4.png",
+    title: "ATENCION AL CLIENTE 24 / 7",
+  },
+];
